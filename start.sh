bc208903d6a73a82ec2001934149856c912b8d6f
Himanshu Yadav <168503095+Himanshu-2678@users.noreply.github.com>
2026-03-03 17:49:33 +0530

    Fixed celery_app worker

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Celery worker in background
-celery -A worker celery_app worker --loglevel=info &
+celery -A worker:celery_app worker --loglevel=info &
 
 # Start Gunicorn in foreground
 gunicorn app:app --bind 0.0.0.0:$PORT
